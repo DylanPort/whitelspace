46b8f40e756057fcb819a78919e3449d0ab8cdf4
jack9757 <tua-email@example.com>
2025-12-05 15:18:49 +0100

    add Google Ads tracking to links site

--- providers/links-site/app/layout.js
+++ providers/links-site/app/layout.js
@@ -11,6 +11,20 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        {/* Google Ads Tag */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=AW-17782201837"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'AW-17782201837');
+            `,
+          }}
+        />
+      </head>
       <body>{children}</body>
     </html>
   )
